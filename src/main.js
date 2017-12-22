44fab62b9047db01519f82511a1018f3917f8ecf
zhangzy <zhangzeyu@bilibili.com>
2017-12-22 14:21:34 +0800

    删除store

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import store from './store'
 import { sync } from 'vuex-router-sync'
 
 Vue.config.productionTip = false
